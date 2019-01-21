8306671ce7eff7abcb7f1b1903522a5e2a19ceb9
yong liang <yongliang24@gmail.com>
2019-01-21 19:39:51 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
 
+def my_collect(array)
+  i=0
+  empty_array
+  while i<array.length
+    name = array[i]
+    name.split(" ").first
+    empty_array<<name
 
+    yield(array[i])
+  end
+  return empty_array
+end
